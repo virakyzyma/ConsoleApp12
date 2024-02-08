5a0469f89229da8512908fb0a79f4aa65969247f
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-02-08 15:05:45 +0200

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number of cubes: ");
+            int cub = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Count -> { Result(cub)}");
+        }
+        static int Result(int a, int i = 2, int count = 0)
+        {
+            if(a>0)
+            {
+                return Result(a-i, i+1, count + 1);
+            }
+            else
+            {
+                return count;
+            }
+        }
+    }
+}
